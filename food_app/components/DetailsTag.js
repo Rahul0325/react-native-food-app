22d0f502964c9550dd8221255acfef8b479e46c4
Rahul Kalaria <56820854+Rahul0325@users.noreply.github.com>
2023-01-21 16:17:47 +0100

    details screen tag added

--- food_app/components/DetailsTag.js
+++ food_app/components/DetailsTag.js
@@ -1,8 +1,33 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const detailsTag = () => {
-  return <View>detailsTag</View>;
+const detailsTag = (props) => {
+  return (
+    <View style={styles.background}>
+      <Image style={styles.icon} source={props.uri}></Image>
+      <Text style={styles.text}>{props.text}</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#D9D9D9",
+    borderRadius: 10,
+    margin: 10,
+    maxWidth: 70,
+  },
+  icon: {
+    resizeMode: "cover",
+    marginTop: 5,
+  },
+  text: {
+    margin: 5,
+    textAlign: "center",
+  },
+});
 
 export default detailsTag;
