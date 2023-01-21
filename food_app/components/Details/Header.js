5c19d2706d3bfb341a6dbcd2f527b69c2131a05f
Naval Tapan Bhagat <59922532+navalBhagat@users.noreply.github.com>
2023-01-21 16:36:45 +0100

    Added the food iamge to the header! :)

--- food_app/components/Details/Header.js
+++ food_app/components/Details/Header.js
@@ -3,10 +3,13 @@ import { View, Image, Text, StyleSheet } from 'react-native'
 
 const Header = (props) => {
   return (
-    <View style={styles.detailsHeader}>
-        <View style={styles.backButton}><Image source={require("../../assets/icons/Back.png")}/></View>
-        <View><Text style={styles.headerText}>{props.name}</Text></View>
-    </View>
+    <>
+        <View style={styles.detailsHeader}>
+            <View style={styles.backButton}><Image source={require("../../assets/icons/Back.png")}/></View>
+            <View><Text style={styles.headerText}>{props.name}</Text></View>
+        </View>
+        <View><Image source={styles.source_food} style={styles.foodImg}/></View>
+    </>
   )
 }
 
@@ -22,6 +25,17 @@ const styles = StyleSheet.create({
     backButton: {
         justifyContent: 'center',
     },
+    source_food: {
+        uri: "https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000",
+    },
+    foodImg: {
+        backgroundColor: "red",
+        height: "65%",
+        width: "100%",
+        borderRadius: 20,
+        marginTop: 20,
+        resizeMode: "cover",
+    }
 })
 
 export default Header
